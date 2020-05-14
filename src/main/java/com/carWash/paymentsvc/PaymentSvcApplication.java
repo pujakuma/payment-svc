eb017d6e5f98671963045af204c39cdfff7602c7
PUJA SINGH <pujasa25@gmail.com>
2020-05-14 10:12:01 +0530

    payment svc for crawash app to repo

---
+++ src/main/java/com/carWash/paymentsvc/PaymentSvcApplication.java
@@ -0,0 +1,13 @@
+package com.carWash.paymentsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSvcApplication.class, args);
+	}
+
+}
